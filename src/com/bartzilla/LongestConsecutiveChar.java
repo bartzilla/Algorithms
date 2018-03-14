1a4c09ee3959f33efe1005bad4f40b1bfeca10cc
Cipriano Sanchez <ciprianosanchez@gmail.com>
2018-03-14 10:02:39 +0100

    This is actually a better answer for the char occurrences problem

--- src/com/bartzilla/LongestConsecutiveChar.java
+++ src/com/bartzilla/LongestConsecutiveChar.java
@@ -7,26 +7,27 @@ public class LongestConsecutiveChar {
 
     public static void main(String[] args) {
 
-        System.out.println(LongestConsecutiveChar.getLongestConsecutiveChar("AAAAAABBBBCDEAAA"));
+        System.out.println(LongestConsecutiveChar.getLongestConsecutiveChar("AAAAAABBBBCDEAA"));
     }
 
     static Map<Character, Integer> getLongestConsecutiveChar(String value) {
         int maxCount = 1;
-        int counter = 1;
+        char maxChar = value.charAt(0);
         char currentChar = value.charAt(0);
-        char maxChar = currentChar;
+        int counter = 1;
 
         for (int i = 1; i < value.length(); i++) {
 
             if (currentChar == value.charAt(i)) {
                 counter++;
+
+                if (counter > maxCount) {
+                    maxCount = counter;
+                    maxChar = currentChar;
+                }
+
             } else {
                 counter = 1;
-            }
-
-            if (counter > maxCount) {
-                maxCount = counter;
-                maxChar = currentChar;
             }
 
             currentChar = value.charAt(i);
